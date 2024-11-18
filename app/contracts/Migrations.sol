2a67f0ed6f0d48d2a5da6a3b179c75f7a0a1f064
Shrutiiiii <147747068+shrutzzz01@users.noreply.github.com>
2024-11-18 23:01:34 +0530

    Update Migrations.sol

--- app/contracts/Migrations.sol
+++ app/contracts/Migrations.sol
@@ -1,1 +1,21 @@
+pragma solidity ^0.5.1;
 
+contract Mirgations{
+  address public owner;
+  uint public last_comapleted_migration;
+  modifier restricted() {
+    if (msg.sender == owner) _;
+  }
+
+  constructor() public {
+    owner = msg.sender;
+  }
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
+
+  function upgrade(address new_address) public restricted {
+    Migrations upgraded = Migrations(new_address);
+    upgraded.setCompleted(last_completed_migration);
+  }
+}
